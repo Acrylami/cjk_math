177de6042f2923e6de53bc4001210c56c14a9a9e
Alena Miles <acrylami@gmail.com>
2016-10-15 20:25:50 +0100

    Uncommented code
    Uncommented all the code I could find, but I haven't yet figured out how
    to get gofmt to work...

--- cjk_math.go
+++ cjk_math.go
@@ -395,16 +395,16 @@ func MulOverflowUint64(a, b uint64) bool {
 
 
 
-// func rountFloat64(val float64, roundOn float64, places int ) (newVal float64) {
-// 	var round float64
-// 	pow := math.Pow(10, float64(places))
-// 	digit := pow * val
-// 	_, div := math.Modf(digit)
-// 	if div >= roundOn {
-// 		round = math.Ceil(digit)
-// 	} else {
-// 		round = math.Floor(digit)
-// 	}
-// 	newVal = round / pow
-// 	return
-// }
+func rountFloat64(val float64, roundOn float64, places int ) (newVal float64) {
+ 	var round float64
+ 	pow := math.Pow(10, float64(places))
+ 	digit := pow * val
+ 	_, div := math.Modf(digit)
+ 	if div >= roundOn {
+ 		round = math.Ceil(digit)
+ 	} else {
+ 		round = math.Floor(digit)
+ 	}
+ 	newVal = round / pow
+ 	return
+ }
